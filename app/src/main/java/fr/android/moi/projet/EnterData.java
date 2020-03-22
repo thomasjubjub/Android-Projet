3eb18cc102e056d3625ddb99828b6e8dbf13df1b
Thomas <tjlenfer@gmail.com>
2020-03-22 13:28:26 +0100

    le commencement

---
+++ app/src/main/java/fr/android/moi/projet/EnterData.java
@@ -0,0 +1,14 @@
+package fr.android.moi.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_data);
+    }
+}
---
+++ app/src/main/java/fr/android/moi/projet/MainActivity.java
@@ -0,0 +1,37 @@
+package fr.android.moi.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button nouveauMatch;
+    private Button historique;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nouveauMatch=(Button) findViewById(R.id.newMatch);
+        historique=(Button) findViewById(R.id.historique);
+
+        nouveauMatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openData();
+            }
+        });
+    }
+
+    public void openData()
+    {
+        Intent intent = new Intent(this, EnterData.class);
+        startActivity(intent);
+    }
+
+
+}
