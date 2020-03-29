50a1d2f4725e79b5fc96070af9bb73f005d5796f
Zow <zcompoint@gmail.com>
2020-03-29 17:28:04 +0200

    before merge

--- app/src/main/java/fr/android/moi/projet/Historique.java
+++ app/src/main/java/fr/android/moi/projet/Historique.java
@@ -1,9 +1,13 @@
 package fr.android.moi.projet;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import java.util.ArrayList;
@@ -13,15 +17,26 @@ public class Historique extends AppCompatActivity extends ActionBarActivity {
 
     private DatabaseManager databaseManager;
     private List<Match> matches;
-    private TextView textHistorique;
-    private ListView mListView;
+    private Button buttonMatch1;
+    private Button buttonMatch2;
+    private Button buttonMatch3;
+    private Button buttonMatch4;
+    private Button buttonMatch5;
+    private ImageButton retour;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historique);
 
-        mListView = (ListView) findViewById(R.id.listView);
+        buttonMatch1 = (Button) findViewById(R.id.match1);
+        buttonMatch2 = (Button) findViewById(R.id.match2);
+        buttonMatch3 = (Button) findViewById(R.id.match3);
+        buttonMatch4 = (Button) findViewById(R.id.match4);
+        buttonMatch5 = (Button) findViewById(R.id.match5);
+        retour = (ImageButton) findViewById(R.id.imageBack);
+
 
         // Récuperation des infos dans la bdd
         databaseManager = new DatabaseManager(this);
@@ -29,10 +44,7 @@ public class Historique extends AppCompatActivity extends ActionBarActivity {
         Log.d("InHistorique", matches.toString());
         databaseManager.close();
 
-        // creation adapter
-        MatchAdapter adapter = new MatchAdapter(Historique.this, matches);
-        mListView.setAdapter(adapter);
-
+        Intent intent = getIntent();
 
     }
 }
